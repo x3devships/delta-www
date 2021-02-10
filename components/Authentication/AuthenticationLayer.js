3bcc7a2f2349cf96264e795485238e38533f0765
0xRevert <coinphd@protonmail.com>
2021-02-09 17:46:58 -0800

    Pass off

--- components/Authentication/AuthenticationLayer.js
+++ components/Authentication/AuthenticationLayer.js
@@ -3,27 +3,27 @@ import { useEffect, useState } from 'react';
 const AuthenticationLayer = ({ children }) => {
   const [authenticated, setAuthenticated] = useState();
 
-  useEffect(() => {
-    const authenticate = async password => {
-      const response = await fetch(`${origin}/api/authenticate`, {
-        headers: { Authorization: password }
-      });
+  // useEffect(() => {
+  //   const authenticate = async password => {
+  //     const response = await fetch(`${origin}/api/authenticate`, {
+  //       headers: { Authorization: password }
+  //     });
 
-      const authenticated = response.status === 200;
-      if (!authenticated) {
-        // eslint-disable-next-line no-alert
-        localStorage.password = prompt('Access denied', '');
-        // eslint-disable-next-line no-restricted-globals
-        location.reload();
-      }
+  //     const authenticated = response.status === 200;
+  //     if (!authenticated) {
+  //       // eslint-disable-next-line no-alert
+  //       localStorage.password = prompt('Access denied', '');
+  //       // eslint-disable-next-line no-restricted-globals
+  //       location.reload();
+  //     }
 
-      setAuthenticated(authenticated);
-    };
+  //     setAuthenticated(authenticated);
+  //   };
 
-    authenticate(localStorage.password);
-  }, []);
+  //   authenticate(localStorage.password);
+  // }, []);
 
-  return <>{authenticated === true ? children : <></>}</>;
+  return <>{children}</>
 };
 
 export default AuthenticationLayer;
