f28a918c0f573ac239ff2e1febe881d879fba727
notsegfault <notsegfault@gmail.com>
2021-02-20 19:01:52 -0500

    Layout changes

--- components/Staking/Staking.js
+++ components/Staking/Staking.js
@@ -110,49 +110,47 @@ const Staking = ({ onWalletConnect }) => {
   const getContributionConfirmationDetailsContent = details => {
     return <div>
       <div className="text-base">
-        <span className="inline-block font-medium">Your Contribution: </span>
-        <span className="inline-block text-green-500">{details.baseContributionAmount.toLocaleString()} ETH</span>
+        <span className="block font-medium">Your Contribution:</span>
+        <span className="block text-green-500">{details.baseContributionAmount.toLocaleString()} ETH</span>
       </div>
       <div className="text-base">
-        <span className="inline-block font-medium">Early Signup Bonus ({(details.timeBonus * 100).toFixed(2)}%): </span>
-        <span className="inline-block text-green-500">{details.timeBonusAmount.toLocaleString()} ETH</span>
+        <span className="block font-medium">Early Signup Bonus ({(details.timeBonus * 100).toFixed(2)}%):</span>
+        <span className="block text-green-500">+ {details.timeBonusAmount.toLocaleString()} ETH</span>
       </div>
       {details.refCode > 0 &&
         <div className="text-base">
-          <span className="inline-block font-medium">Referral Signup Bonus ({(details.referralBonus * 100).toFixed(2)})%: </span>
-          <span className="inline-block text-green-500">{details.referralBonusAmount.toLocaleString()} ETH</span>
+          <span className="block font-medium">Referral Signup Bonus ({(details.referralBonus * 100).toFixed(2)}%):</span>
+          <span className="block text-green-500">+ {details.referralBonusAmount.toLocaleString()} ETH</span>
         </div>}
       <hr />
-      <div className="text-base">
-        <span className="inline-block font-medium">Total contribution Value: </span>
-        <span className="inline-block text-green-500">{details.total.toLocaleString()} ETH</span>
-      </div>
     </div>
   };
 
   const getContributionReceiptContent = (details, txId) => {
+    const shortTxId = `${txId.substring(0, 8)}...${txId.substring(txId.length - 7, txId.length - 1)}`;
+
     return <div>
       <div className="text-base">
-        <span className="inline-block font-medium">Your Contribution: </span>
-        <span className="inline-block">{details.baseContributionAmount.toLocaleString()} ETH</span>
+        <span className="block font-medium">Your Contribution:</span>
+        <span className="block">{details.baseContributionAmount.toLocaleString()} ETH</span>
       </div>
       <div className="text-base">
-        <span className="inline-block font-medium">Early Signup Bonus ({(details.timeBonus * 100).toFixed(2)}%): </span>
-        <span className="inline-block">+ {details.timeBonusAmount.toLocaleString()} ETH</span>
+        <span className="block font-medium">Early Signup Bonus ({(details.timeBonus * 100).toFixed(2)}%):</span>
+        <span className="block">{details.timeBonusAmount.toLocaleString()} ETH</span>
       </div>
       {details.refCode > 0 &&
         <div className="text-base">
-          <span className="inline-block font-medium">Referral Signup Bonus ({(details.referralBonus * 100).toFixed(2)})%: </span>
-          <span className="inline-block">+ {details.referralBonusAmount.toLocaleString()} ETH</span>
+          <span className="block font-medium">Referral Signup Bonus ({(details.referralBonus * 100).toFixed(2)}%):</span>
+          <span className="block">{details.referralBonusAmount.toLocaleString()} ETH</span>
         </div>}
       <hr />
       <div className="text-base">
-        <span className="inline-block font-medium">Total contribution Value: </span>
-        <span className="inline-block">{details.total.toLocaleString()} ETH</span>
+        <span className="block font-medium">Total contribution Value:</span>
+        <span className="block">{details.total.toLocaleString()} ETH</span>
       </div>
       <div className="text-base">
-        <span className="inline-block font-medium">Transaction Id: </span>
-        <span><a className="underline" target="_blank" rel="noopener noreferrer" href={`https://etherscan.io/tx/${txId}`}>{txId}</a></span>
+        <span className="block font-medium">Transaction Id:</span>
+        <span className="block font-medium"><a className="underline" target="_blank" rel="noopener noreferrer" href={`https://etherscan.io/tx/${txId}`}>{shortTxId}</a></span>
       </div>
     </div>
   };
@@ -212,11 +210,8 @@ const Staking = ({ onWalletConnect }) => {
       transactionMessage.close();
 
       await modalContext.showMessage('Congratulations!', <>
-        <div className="text-base mt-2">
-          You have successfully contributed your Ethereum. You will receive your rLP after the Limited Staking Window has closed.
-        </div>
-        <hr />
-        <div className="mt-5">
+        <div className="text-lg">You will be able to claim your rLP tokens after the Limited Staking Window has closed.</div>
+        <div className="mt-4">
           {getContributionReceiptContent(contributionDetails, receipt.transactionHash)}
         </div>
       </>)
@@ -225,6 +220,38 @@ const Staking = ({ onWalletConnect }) => {
     }
 
     return Promise.resolve();
+  };
+
+  const renderContributeForm = () => {
+    return <>
+      <div className="grid gap-2 grid-cols-2 md:flex">
+        <div className="bg-white flex border border-black">
+          <div className="p-3">
+            <Input
+              value={ethAmountText}
+              placeholder='0.00'
+              onChange={onEthAmountChanged}
+              valid={validEthAmount}
+              className="border-transparent text-xl"
+              style={{ borderBottom: '1px solid black' }}
+            />
+          </div>
+          <div className="pr-3 text-sm self-end mb-3">ETH</div>
+        </div>
+        <div className="p-1 max-w-max border border-black">
+          <Button onClick={() => onMaxEthAmount()} className="bg-gray-400 h-full ring-pink-300 ring-inset focus:bg-gray-400">
+            <span className="uppercase">{t('max')}</span>
+          </Button>
+        </div>
+      </div>
+      <TransactionButton
+        text="Contribute"
+        textLoading="Contributing..."
+        secondaryLooks
+        disabled={lswStats.timeStart === DATA_UNAVAILABLE}
+        onClick={onContribute}
+      />
+    </>;
   };
 
   return (
@@ -241,21 +268,22 @@ const Staking = ({ onWalletConnect }) => {
                   </div>
                   <ProgressBarCountDown lswStats={lswStats} />
                 </div>
-                <div className="h-4/6 border-2 pt-2 border-black border-t-0">
-                  <div className="m-auto w-11/12 text-4xl py-9 font-wulkan">{t('limitedStaking')}</div>
-                  <div className="m-auto w-11/12 text-xl py-2 font-wulkan">
-                    {t('yourContribution')} {lswStats.data.totalEthContributed}
+                <div className="h-4/6 border-2 pt-2 border-black border-t-0 p-2">
+                  <div className="m-auto text-4xl py-9 font-wulkan">{t('lswExplanationTitle')}</div>
+                  <div className="m-auto">
+                    {t('lswExplanationContent')}
                   </div>
-                  <div className="flex space-x-160 sm:flex-wrap sm:space-x-0">
-                    <iframe style={{
-                      marginLeft: 'auto',
-                      marginRight: 'auto'
-                    }} title="contribution" src="https://duneanalytics.com/embeds/20459/42016/MCZSRgV5KrBby66NVZpKK7FxOdTHxg0JEJecWbu9" width="95%" height="391" />
+                  <iframe style={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto'
+                  }} title="contribution" src="https://duneanalytics.com/embeds/20459/42016/MCZSRgV5KrBby66NVZpKK7FxOdTHxg0JEJecWbu9" width="100%" height="391" />
+                  <div className="m-auto text-xl py-2 text-center">
+                    <div className="font-bold">{t('yourContribution')}</div><div>{lswStats.data.totalEthContributed.toLocaleString()} ETH</div>
                   </div>
                 </div>
                 <div className="flex h-128 border-2 pt-2 border-black border-t-0 pl-9 sm:block sm:pb-5 sm:pl-2">
-                  <div className="flex-1 m-auto w-11/12 py-9 pl-9 mb-9 sm:pl-0">
-                    <div className="text-4xl pb-4 font-wulkan">{t('bonus')}</div>
+                  <div className="flex-1 m-auto py-9 pl-9 mb-9 sm:pl-0">
+                    <div className="text-4xl pb-4 font-wulkan">{t('contribute')}</div>
                     <div className="pb-2" dangerouslySetInnerHTML={{ __html: t('earnWithDelta') }} />
                     <div>
                       {connectWalletVisible ? (
@@ -273,43 +301,7 @@ const Staking = ({ onWalletConnect }) => {
                           <span>{t('connectWallet')}</span>
                           <img alt="+" src={plus} className="m-auto pl-8" />
                         </Button>
-                      ) : (
-                          <div className="flex w-6/12">
-                            <Input
-                              value={ethAmountText}
-                              style={{
-                                border: '0.063rem solid black',
-                                marginRight: '0.313rem',
-                                marginTop: '0.938rem',
-                                minWidth: '45%',
-                                backgroundColor: 'transparent'
-                              }}
-                              onChange={onEthAmountChanged}
-                              valid={validEthAmount}
-                            />
-                            <Button
-                              onClick={() => onMaxEthAmount()}
-                              style={{
-                                marginRight: '1px',
-                                borderRadius: '0px',
-                                backgroundColor: 'gray',
-                                padding: '1rem',
-                                marginTop: '1rem'
-                              }}
-                              type="number"
-                              className="p-4 mt-4 inline-block text-white uppercase flex ml-2"
-                            >
-                              <span className="uppercase">{t('max')}</span>
-                            </Button>
-                            <TransactionButton
-                              text="Contribute"
-                              textLoading="Contributing..."
-                              secondaryLooks
-                              disabled={lswStats.timeStart === DATA_UNAVAILABLE}
-                              onClick={onContribute}
-                            />
-                          </div>
-                        )}
+                      ) : renderContributeForm()}
                     </div>
                     {!validEthAmount && <HelperText valid={false}>{t('validContribution')}</HelperText>}
                   </div>
