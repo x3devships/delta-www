c9136641ac094a600556dc91cc30e7e224841257
notsegfault <notsegfault@gmail.com>
2021-02-24 20:58:57 -0500

    replaced recharts with victory charts

--- components/ReferralProgram/ReferralProgram.js
+++ components/ReferralProgram/ReferralProgram.js
@@ -5,48 +5,47 @@
 import { useContext, useEffect, useState } from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import { useWallet } from 'use-wallet';
-import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-import moment from 'moment';
+import { VictoryChart, VictoryGroup, VictoryArea } from 'victory';
 import { DeltaPanel, DeltaSection } from '../Section';
 import { DeltaTitleH3 } from '../Title';
 import { formatting } from '../../helpers';
 import { ConnectWalletButton } from '../Buttons';
 import useReferralRewardsChartData from '../../hooks/useReferralRewardsChartData';
+import { DATA_UNAVAILABLE } from '../../config';
 
 const ReferralProgram = ({ lswStats }) => {
   const wallet = useWallet();
   const { t } = useTranslation('home');
   const chartData = useReferralRewardsChartData();
 
-  const formatXAxis = (tickItem) => {
-    return moment(tickItem).format('MMM Do YY');
-  };
-
   return <DeltaSection title={t('deltaReferral')}>
     <DeltaPanel>
-      <div className="block md:grid md:grid-cols-2 md:gap-6">
+      <div className="block">
         <div className="md:mt-0">
           {!wallet?.account ? <ConnectWalletButton /> :
             <>
               <DeltaTitleH3>Your Referral Bonus</DeltaTitleH3>
 
-              <ResponsiveContainer className="mt-6" aspect={1} height="75%">
-                <AreaChart
-                  data={chartData.data}
-                  margin={{
-                    top: 0,
-                    right: 0,
-                    left: 0,
-                    bottom: 0,
-                  }}
-                >
-                  <XAxis interval={24} dataKey="date" tickFormatter={formatXAxis} />
-                  <YAxis dataKey="referralBonusWETH" name="ETH" />
-                  <Tooltip label="ETH" labelFormatter={() => 'ETH'} />
-                  <Area scale="time" dataKey="referralBonusWETH" stroke="#8884d8" fill="#8884d8" />
-                </AreaChart >
-              </ResponsiveContainer>
-
+              {lswStats.data.referralBonusWETH !== DATA_UNAVAILABLE && lswStats.data.referralBonusWETH > 0 ?
+                <div className="w-full md:w-8/12">
+                  <VictoryChart padding={34} width={400} height={400} scale={{ x: "time", y: "linear" }} minDomain={{ y: 0 }}>
+                    <VictoryGroup
+                      style={{
+                        data: { strokeWidth: 1, fillOpacity: 0.6 }
+                      }}
+                    >
+                      <VictoryArea
+                        style={{
+                          data: { fill: "#b794f4", stroke: "#b794f4" }
+                        }}
+                        x={d => new Date(d.date)}
+                        y="referralBonusWETH"
+                        data={chartData.data}
+                      />
+                    </VictoryGroup>
+                  </VictoryChart>
+                </div>
+                : <></>}
               <DeltaPanel>
                 <ul className="list-disc list-inside">
                   <li>ETH earned: {formatting.getTokenAmount(lswStats.data.referralBonusWETH, 0, 8)}</li>
