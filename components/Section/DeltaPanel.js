b0039125375cdb0480cd4b6a9aafe516a9311cdd
notsegfault <notsegfault@gmail.com>
2021-03-01 14:38:42 -0500

    fixed delta panel require wallet connected

--- components/Section/DeltaPanel.js
+++ components/Section/DeltaPanel.js
@@ -1,6 +1,19 @@
-const DeltaPanel = ({ children, className }) => {
+import { useWallet } from 'use-wallet';
+import { ConnectWalletButton } from '../Buttons';
+
+const DeltaPanel = ({ children, className, requiresConnectedWallet }) => {
+  const wallet = useWallet();
+
+  const renderContent = () => {
+    if (requiresConnectedWallet && !wallet?.account) {
+      return <ConnectWalletButton />
+    }
+
+    return children;
+  }
+
   return <div className={`${className || ''} w-full`}>
-    {children}
+    {renderContent()}
   </div>;
 }
 
