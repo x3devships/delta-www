7bbfdda622042de45293c87c41e81ce32bb77727
notsegfault <notsegfault@gmail.com>
2021-03-01 09:43:27 -0500

    wip

--- components/Vault/VaultStaking.js
+++ components/Vault/VaultStaking.js
@@ -4,45 +4,47 @@ import { useState } from 'react';
 import DeltaButton from '../Button/DeltaButton';
 import { DeltaPanel } from '../Section'
 
-const VaultDeposit = () => {
+const VaultDeposit = ({ token }) => {
   const [toggle, setToggle] = useState(false);
 
-  return <div className="flex flex-grow">
-    <div className="border border-black p-1 flex flex-row">
-      <DeltaButton onClick={() => setToggle(t => !t)} grayLook={toggle}>Stake</DeltaButton>
-      <DeltaButton onClick={() => setToggle(t => !t)} grayLook={!toggle}>Buy</DeltaButton>
+  return <DeltaPanel className="flex items-center text-center flex-wrap">
+    <div className="flex border border-black p-1 flex-grow md:flex-none">
+      <DeltaButton className="flex-1 mr-2 md:flex-grow-0" onClick={() => setToggle(t => !t)} grayLook={toggle}>Stake</DeltaButton>
+      <DeltaButton className="flex-1 md:flex-grow-0" onClick={() => setToggle(t => !t)} grayLook={!toggle}>Buy</DeltaButton>
     </div>
-  </div>;
+  </DeltaPanel>;
 };
 
-const VaultWithdraw = () => {
+const VaultWithdraw = ({ token }) => {
   const [toggle, setToggle] = useState(false);
 
-  return <div className="flex flex-grow">
-    <div className="border border-black p-1 flex flex-row">
-      <DeltaButton onClick={() => setToggle(t => !t)} grayLook={toggle}>Stake</DeltaButton>
-      <DeltaButton onClick={() => setToggle(t => !t)} grayLook={!toggle}>Buy</DeltaButton>
-      <DeltaButton onClick={() => setToggle(t => !t)} grayLook={!toggle}>Buy</DeltaButton>
-      <DeltaButton onClick={() => setToggle(t => !t)} grayLook={!toggle}>Buy</DeltaButton>
+  return <DeltaPanel className="flex items-center text-center flex-wrap">
+    <div className="flex border border-black p-1 flex-grow md:flex-none">
+      <DeltaButton className="flex-1 mr-2 md:flex-grow-0" onClick={() => setToggle(t => !t)} grayLook={toggle}>Stake</DeltaButton>
+      <DeltaButton className="flex-1 md:flex-grow-0" onClick={() => setToggle(t => !t)} grayLook={!toggle}>Buy</DeltaButton>
     </div>
-  </div>;
+  </DeltaPanel>;
 };
 
 const VaultStaking = ({ token, className }) => {
-  const [toggle, setToggle] = useState(false);
+  const [depositAction, setDepositAction] = useState(true);
 
-  return <DeltaPanel className={`pt-1 border-t-2 border-dashed border-black ${className || ''}`}>
-    <div className="flex uppercase" onClick={() => setToggle(t => !t)}>
+  const showAllWithdrawalContracts = () => {
+    setDepositAction(false);
+  };
+
+  return <DeltaPanel className={`pt-2 border-t-2 border-dashed border-black ${className || ''}`}>
+    <div className="flex uppercase" onClick={() => setDepositAction(t => !t)}>
       <div className="flex flex-grow" />
       <div className="flex self-end select-none cursor-pointer text-sm">
-        <div className={`${toggle ? 'text-gray-300' : 'text-black'}`}>Deposit</div>
+        <div className={`${!depositAction ? 'text-gray-300' : 'text-black'}`}>Deposit</div>
         <div className="px-1">/</div>
-        <div className={`${!toggle ? 'text-gray-300' : 'text-black'}`}>Withdraw</div>
+        <div className={`${depositAction ? 'text-gray-300' : 'text-black'}`}>Withdraw</div>
       </div>
     </div>
     <div className="mt-4 md:mt-1">
-      {toggle ? <VaultWithdraw /> : <VaultDeposit />}
-      {/* <DeltaButton>See all withdrawal contracts</DeltaButton> */}
+      {depositAction ? <VaultDeposit /> : <VaultWithdraw />}
+      <DeltaButton className="mt-4" onClick={showAllWithdrawalContracts}>See all withdrawal contracts</DeltaButton>
     </div>
   </DeltaPanel>
 };
