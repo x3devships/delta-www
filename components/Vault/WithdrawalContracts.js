3a21a62d3909dd5a97cfeab20af5a99733e561ee
notsegfault <notsegfault@gmail.com>
2021-03-01 22:12:58 -0500

    wip

--- components/Vault/WithdrawalContracts.js
+++ components/Vault/WithdrawalContracts.js
@@ -3,13 +3,13 @@ import { GlobalHooksContext } from '../../contexts/GlobalHooks';
 import { DeltaPanel } from '../Section';
 
 const WithdrawalContractItem = ({ contract }) => {
-  return <Delta>
-    };
+  return <DeltaPanel />;
+};
 
 const WithdrawalContracts = () => {
   const globalHooks = useContext(GlobalHooksContext);
 
   return <DeltaPanel>
-      {globalHooks.staking.withdrawalContracts.map((contract, index) => <WithdrawalContractItem key={`contract-${index}`} contract={contract} />)}
-    </DeltaPanel>
+    {globalHooks.staking.withdrawalContracts.map((contract, index) => <WithdrawalContractItem key={`contract-${index}`} contract={contract} />)}
+  </DeltaPanel>
 };
