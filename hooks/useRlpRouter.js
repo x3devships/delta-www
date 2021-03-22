0b44ced972b8d66d52e70e6b234a00def3ac33c3
dev-eth <66653006+dev-eth@users.noreply.github.com>
2021-03-22 14:08:31 -0400

    Update useRlpRouter.js
    Update function modified

--- hooks/useRlpRouter.js
+++ hooks/useRlpRouter.js
@@ -117,7 +117,7 @@ const useRlpRouter = () => {
   const update = async () => {
     if (!wallet) return;
 
-    const { minLpAmount, gasEstimation } = await mint(false);
+    const { minLpAmount, gasEstimation } = await mint(true);
 
     setEstimatedRlpAmount(minLpAmount?.toString() / 1e18);
     setGasEstimation(gasEstimation);
