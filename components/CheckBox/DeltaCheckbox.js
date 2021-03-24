03c9ae0ccca9dc60651fc8f73df648ec35827a89
dev-eth <diego094.dev@gmail.com>
2021-03-24 09:50:06 -0400

    remove comment from DeltaCheckbox

--- components/CheckBox/DeltaCheckbox.js
+++ components/CheckBox/DeltaCheckbox.js
@@ -6,7 +6,6 @@ const DeltaCheckbox = ({ onChange, className = '' }) => {
 
   return <div className={`py-4 bg-lightgray bg-gray-300 cursor-pointer ${className}`}>
     <Input id={inputId} className="cursor-pointer border ml-4 mr-2 border-solid border-black" type="checkbox" onChange={onChange} />
-    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
     <label htmlFor={inputId} className="pr-4 uppercase select-none cursor-pointer">DFV Check</label>
   </div>
 }
