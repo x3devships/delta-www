ab47e65d85bb167674657347ed33a7cc23ae465e
nd84 <your_email@youremail.com>
2021-06-03 22:36:49 +0300

    no message

--- helpers/formatting.js
+++ helpers/formatting.js
@@ -3,18 +3,24 @@ import { DATA_UNAVAILABLE } from '../config';
 
 // ND: Introduced this additional method to get the formatted string with the amount since we 
 // can have a claimable amount of ETH which is really small and still claimable.
-const getTokenAmountAsStrWithMinPrecision = (amount, decimals, precision=2 ) => {
+const getTokenAmountAsStrWithMinPrecision = (amount, decimals, precision=2, maxStringLen=4 ) => {
   if (amount === DATA_UNAVAILABLE) {
     return amount;
   }
   if (!BigNumber.isBigNumber(amount)) {
-    console.error('getTokenAmountWithPrecision: Error not big number!');
+    console.error('getTokenAmountAsStrWithMinPrecision: Error not big number!');
     return amount;
   }
   let ret = '0';
-  while( parseFloat(ret) === 0 && precision <= decimals ) {
-    ret = Number.parseFloat( amount.div(new BigNumber(10).pow(decimals)).toString() ).toPrecision(precision);
+  let lastRealAmt = new BigNumber(ret);
+  while( parseFloat(ret) === 0 && precision <= decimals && precision <= maxStringLen ) {
+    const tenToPowDec = new BigNumber(10).pow(decimals);
+    lastRealAmt = amount.div(tenToPowDec);
+    ret = Number.parseFloat( lastRealAmt.toString() ).toPrecision(precision);
     precision++;
+  }
+  if ( lastRealAmt.gt(0) && lastRealAmt.lt(0.001) ) {
+    return `< 0.0001`;
   }
   return ret;
 };
